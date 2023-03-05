ce1b74e1deac9d88e85a4d452b97a78a731b6ee6
tulika-jha <jtulika2017@gmail.com>
2023-03-04 17:11:47 -0800

    added function to simulate trajectory given time series of prices

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import numpy as np
 from q_learning import q_learning
 from utils import simulate_data_from_series
 
@@ -14,12 +15,20 @@ def run_rl(filename, method):
     # dataset = load_and_preprocess_data(filename)
     dataset = pd.read_csv(filename)
     # dataset is a pandas DataFrame with the columns (time, price)
-    m_chain_data = simulate_data_from_series(dataset)
+    m_chain_data = simulate_data_from_series(dataset, num_states_price=100)
+
+    #print(m_chain_data)
+    #print(np.array(m_chain_data).max())
 
     # solve the problem using the specified method
     rl_func = ALGORITHM[method]
-    policy = rl_func(m_chain_data)
-
+    dataset_config = {
+        'state_space_size': 100*100,
+        'action_space_size': 3,
+        'discount_factor': 0.98,
+    }
+    policy = rl_func(data=m_chain_data, dataset_config=dataset_config, learning_rate=0.1)
+    print(policy)
     # evaluate policy
 
 
--- utils.py
+++ utils.py
@@ -1,29 +1,64 @@
 import pandas as pd
+import numpy as np
 
 
-
-
-def simulate_data_from_series(df, num_states=20):
+def simulate_data_from_series(df, num_states_price=100):
     # features to use as states
     # price (discretized), moving average (discretized)
+    ma_window_size = 5
+    df["moving_avg"] = df["price"].rolling(window=5).mean()
     prices = df["price"].values
-    df["price_states"] = (prices - prices.min() * num_states / (prices.max() - prices.min())).floor()
-    df["moving_avg"] = df["prices"].rolling(window=5).mean()
-    df["moving_avg_states"] = (df["moving_avg"].values() - prices.min() * num_states / (prices.max() - prices.min())).floor()
-    moving_avg = df["moving_avg"].values()
+    moving_avg = df["moving_avg"].values
+
+    # since the first (ma_window_size - 1) moving averages are nan
+    prices = prices[ma_window_size-1:]
+    moving_avg = moving_avg[ma_window_size-1:]
+
+    def states_mapping(p, ma):
+        price_mapping = min(np.floor(((p - prices.min())/(prices.max() - prices.min())) * num_states_price), num_states_price - 1)
+        ma_mapping = min(np.floor(((ma - moving_avg.min())/(moving_avg.max() - moving_avg.min())) * num_states_price), num_states_price - 1)
+        return price_mapping * num_states_price + ma_mapping
+
+    def action_mapping(a):
+        action_space = {
+            "buy": 0,
+            "sell": 1,
+            "hold": 2,
+        }
+        return action_space.get(a)
 
     balance = 5
-    # sold = 1 when you have bought a share
-    sold = 0
+    holding = 0 # holding = 1 when you have bought a share and haven't sold it yet
+    holding_price = None
     actions = ["buy","sell","hold"]
 
     markov_chain = []
-    for i in range(len(prices)):
-        prob = np.random.rand()
+    state_curr = states_mapping(prices[0], moving_avg[0])
+
+    for i in range(len(prices) - 1):
+        state_next = states_mapping(prices[i+1], moving_avg[i+1])
         action = "hold"
-        if prices[i] < balance:
-            if prob < 0.5:
+        reward = 0
+        if holding:
+            prob_sell = np.random.rand()
+            if prob_sell < 0.5:
+                action = "sell"
+                reward = prices[i] - holding_price
+                holding = 0
+        else:
+            prob_buy = np.random.rand()
+            if prob_buy < 0.5:
                 action = "buy"
+                holding = 1
+                holding_price = prices[i]
+        action = action_mapping(action)
+        markov_chain.append([int(state_curr), int(action), reward, int(state_next)])
+        state_curr = state_next
+
+    return markov_chain
+
+
+
 
 
 
